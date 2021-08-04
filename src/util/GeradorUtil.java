9e3415a9ec47f71cd70e6e996245459d69bf1e89
karen.rivarola <karen.rivarola@PLE111D24.alunos.sc.senac.br>
2021-08-03 21:59:25 -0300

    Gerando códigos

--- src/util/GeradorUtil.java
+++ src/util/GeradorUtil.java
@@ -41,10 +41,56 @@ public class GeradorUtil {
         return numero;
     }
 
+    /**
+     *
+     * @return 
+     */
+        public String gerarCnpj() {
+        return gerarNumero(2) + "." + gerarNumero(3) + "." + gerarNumero(3) 
+                + "/0001-" + gerarNumero(2);
+
+    }
+    public String gerarCelular() {
+        return "(48) 9" + gerarNumero (4) + "-" + gerarNumero (4);
+    }    
+        
+    public String gerarTelefone() {
+        return "(48)" + gerarNumero (4) + "-" + gerarNumero(4);
+    }
+                  
+    public String gerarCep() {
+        return gerarNumero(5) + "-" + gerarNumero(3);
+    }
+    
+    public String gerarNome() {
+        String[] nomes = {"Silvio", "João", "Maria", "Pedro", "Lucas", "Bruno", 
+            "Karen", "Charlott", "Bianca", "Brayan", "Sueli", "Luciana", "Kamila",
+            "Mariele", "Wilian", "Fatima", "Maximus", "Mateus", "Morgana", 
+            "Karina", "Katherine", "Mayra", "Estela"};
+        int tamanhoLista = nomes.length;
+        int indice = (int) (Math.random() * tamanhoLista);
+                 
+        return nomes [indice] + " " + gerarSobrenome();
+    }
+    
+    private String gerarSobrenome() {
+       String[] sobrenomes = {"Silva", "Gomes", "Oliveira", "Cabrera", "Rivarola", 
+        "Waltrick", "Souza", "Montania", "Santos", "Riera", "Pereira", "Marques", 
+        "Carvalho", "Domingues", "Kegler", "Arruda", "Gamboa", "Ramirez", "Larrea", 
+        "Borel", "Pires", "Abravanel", "Almneida"};
+        int tamanhoLista = sobrenomes.length;
+        int indice = (int) (Math.random() * tamanhoLista);
+                 
+        return sobrenomes [indice];
+        
+    }
+      
     public static void main(String[] args) {
         GeradorUtil util = new GeradorUtil();
         String cpf = util.gerarCpf();
         System.out.println("CPF: " + cpf);
 
+        String nome = util.gerarNome();
+        System.out.println("Nome: " + nome);
     }
 }
